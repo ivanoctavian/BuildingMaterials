09f8cd3fac2afeb8dfdb6c903df7388ad7a5e22d
ivanoctavian <i.octavian900@gmail.com>
2021-01-10 20:53:04 +0200

    login works for angajati, adauga and homePage

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ from flask_mysqldb import MySQL
 import yaml
 import bcrypt
 
-#session['authenticated'] = False
 app = Flask(__name__)
 db = yaml.load(open('db.yaml'))
 app.secret_key = 'oct@vi@n'
@@ -15,29 +14,31 @@ app.config['MYSQL_PASSWORD'] = db['mysql_password']
 app.config['MYSQL_DB'] = db['mysql_db']
 app.config['MYSQL_CURSORCLASS'] = db['mysql_cursorclass']
 mysql = MySQL(app)
-ISLOGIN = False
-@app.route('/home', methods=['GET', 'POST'])
-def index():
-    return render_template('index.html')
+
 
 # ANGAJATI DONE
 @app.route('/angajati', methods=['GET', 'POST'])
-def home():
+def getAngajati():
     print("___app.py/home::angajati: STARTED")
+    if request.method=='GET':
+        if session.get('username') is None:
+            print("Nu e logat")
+            return redirect(url_for('login'))
+        if session['username']:
+            #return render_template('angajati.html')
+            try:
+                curs = mysql.connection.cursor()
+                curs.execute('SELECT * FROM tblAngajati')
+                data = curs.fetchall()
+                # pentru a vedea atributele unui obiect:
+                #for ang in data:
+                 #   print(ang['idAngajat'])
+                print('___app.py / home::angajati: FETCH DONE.')
+                curs.close()
+            except Exception as e:
+                print('ERROR___app.py / home::angajati: EROARE FETCH____', e)
 
-    try:
-        curs = mysql.connection.cursor()
-        curs.execute('SELECT * FROM tblAngajati')
-        data = curs.fetchall()
-        # pentru a vedea atributele unui obiect:
-        #for ang in data:
-         #   print(ang['idAngajat'])
-        print('___app.py / home::angajati: FETCH DONE.')
-        curs.close()
-    except Exception as e:
-        print('ERROR___app.py / home::angajati: EROARE FETCH____', e)
-
-    return render_template('angajati.html', angajati=data)
+            return render_template('angajati.html', angajati=data)
 
 
 
@@ -91,24 +92,21 @@ def adauga():
     return render_template('adauga.html')
 
 
-@app.route('/angajati1', methods=['GET','DELETE'])
-def delete_order():
-    print("___app.py/home::angajati: STARTED")
-    try:
-        curs = mysql.connection.cursor()
-        curs.execute('DELETE FROM tblAngajati WHERE idAngajat = 34')
-        #aasa
-        mysql.connection.commit()
-        curs.close()
-    except Exception as e:
-        print('ERROR___app.py / home::angajati: EROARE FETCH____', e)
-
-    return render_template('angajati.html')
-
-
 @app.route('/editAngajat/<string:id>', methods=['GET', 'POST'])
 def editAngajat(id):
     curs = mysql.connection.cursor()
+    if request.method == "GET":
+        if session.get('username') is None:
+            print("Nu e logat")
+            return redirect(url_for('login'))
+        if session['username']:
+            sql = 'SELECT * FROM tblAngajati WHERE idAngajat=%s'
+            curs.execute(sql, [id])
+            res = curs.fetchone()
+            curs.close()
+            return render_template("editAngajat.html", angajati=res)
+            #return render_template('homePage.html')
+
     if request.method == 'POST':
         try:
             idToUpdate      = id
@@ -120,18 +118,17 @@ def editAngajat(id):
             emailToUpdate   = request.form['email']
             salariuToUpdate = request.form['salariu']
             sql = "UPDATE tblAngajati SET Nume=%s, Prenume=%s, Functie=%s,DataAngajarii=%s,Telefon=%s, Email=%s, SalariuRON=%s WHERE idAngajat=%s"
-            curs.execute(sql,[ numeToUpdate,prenumeToUpdate,functieToUpdate,dataAngToUpd,telefonToUpdate,emailToUpdate,salariuToUpdate, idToUpdate])
+            curs.execute(sql, [numeToUpdate, prenumeToUpdate, functieToUpdate, dataAngToUpd, telefonToUpdate, emailToUpdate, salariuToUpdate, idToUpdate])
             mysql.connection.commit()
             curs.close()
-            return redirect(url_for('home'))
+            return redirect(url_for('getAngajati'))
         except Exception as e:
             print(e)
-
-
-    sql = 'SELECT * FROM tblAngajati WHERE idAngajat=%s'
-    curs.execute(sql,[id])
-    res = curs.fetchone()
-    return render_template("editAngajat.html", angajati=res)
+    # sql = 'SELECT * FROM tblAngajati WHERE idAngajat=%s'
+    # curs.execute(sql,[id])
+    # res = curs.fetchone()
+    # curs.close()
+    # return render_template("editAngajat.html", angajati=res)
 
 # INCERCARE
 def delete(numeidElem,tabel,valoareId):
@@ -150,60 +147,133 @@ def deleteAngajat(id):
         mysql.connection.commit()
         curs.close()
 
-        return redirect(url_for('home'))
+        return redirect(url_for('getAngajati'))
 
 
-@app.route('/register', methods=['GET','POST'])
+@app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'GET':
         return render_template('register.html')
     else:
-        user = request.form['userN']
-        password = request.form['pwd'].encode('utf-8')
-        hash_pwd = bcrypt.hashpw(password, bcrypt.gensalt())
+        session['reg'] = ""
+
+        numeN        = request.form['nume']
+        prenumeN     = request.form['prenume']
+        emailN       = request.form['email']
+        userN        = request.form['username']
+        passwordN    = request.form['pwd2'].encode('utf-8')
+        hash_pwd = bcrypt.hashpw(passwordN, bcrypt.gensalt())
         curs = mysql.connection.cursor()
-        curs.execute('INSERT INTO tblUsers (username, password) VALUES (%s, %s)', ([user, hash_pwd]) )
-        #de facut eroare pentru user already exists
+        try:
+            curs.execute('INSERT INTO tblUsers (Username, Nume, Prenume, Email, Password) VALUES (%s, %s, %s, %s, %s)',
+                         ([userN, numeN, prenumeN, emailN, hash_pwd]))
+            mysql.connection.commit()
+            curs.close()
+            eroareRegister = "OK"
+            session['reg']='OK'
+            #return 'register success'
+            session.clear()
+            return redirect(url_for('login'))
+            #return render_template('register.html', eroareHTML=eroareRegister)
+        except Exception as e:
+            print(e)
+            if e.args[0] == 1062:
+                eroareRegister = "Username already exist. Try another."
+                session['reg'] = '1062'
+                #return render_template('register.html', eroareHTML='MUIE')
+                #return 'username already exists'
+                return redirect(url_for('register'))
+                #return render_template('register.html')
+    #return render_template('register.html', eroareHTML='MUIE')
 
-        mysql.connection.commit()
-        session['username'] = user
-        return redirect(url_for('home'))
+       # session['username'] = userN
+        #return redirect(url_for('homePage'))
+
+@app.route('/homePage', methods=['GET', 'POST'])
+def homePage():
+    if request.method=="GET":
+        if session.get('username') is None:
+            print("Nu e logat")
+            return redirect(url_for('login'))
+        if session['username']:
+            return render_template('homePage.html')
+    # if request.method == 'GET':
+    #     return render_template('homePage.html')
+    # else:
+    #     userLogin = request.form['userLog']
+    #     passLogin = request.form['pwdLog'].encode('utf-8')
+    #     curs = mysql.connection.cursor()
+    #
+    #     curs.execute("SELECT * FROM tblUsers WHERE Username=%s", ([userLogin]))
+    #     userInfo = curs.fetchone()
+    #
+    #     curs.close()
+    #     # print(userInfo['password'])
+    #     session['para'] = ""
+    #     if len(userLogin) > 0:
+    #         if userInfo == None:
+    #             session['para'] = "NOTEXIST"
+    #             return redirect(url_for('homePage'))
+    #
+    #             #return 'user doesnt exist'
+    #         if bcrypt.hashpw(passLogin, userInfo['Password'].encode('utf-8')) == userInfo['Password'].encode('utf-8'):
+    #             session['username'] = userInfo['Username']
+    #             session['authenticated'] = True
+    #             return redirect(url_for('homePage'))
+    #         else:
+    #             session['para'] = "FAILED"
+    #             return redirect(url_for('homePage'))
+    #             #return 'login failed'
 
 
-@app.route('/login', methods=['GET','POST'])
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'GET':
-        return render_template('login.html')
+
+        if session.get('username') is None:
+            print("Nu e logat")
+            return render_template('login.html')
+        if session['username']:
+            return redirect(url_for('homePage'))
+        #else:
+            #return render_template('login.html')
+           #return render_template('login.html')
     else:
+        #session['username'] = ""
         userLogin = request.form['userLog']
         passLogin = request.form['pwdLog'].encode('utf-8')
         curs = mysql.connection.cursor()
 
-        curs.execute("SELECT * FROM tblUsers WHERE username=%s", ([userLogin]))
+        curs.execute("SELECT * FROM tblUsers WHERE Username=%s", ([userLogin]))
         userInfo = curs.fetchone()
 
         curs.close()
         #print(userInfo['password'])
 
         if len(userLogin) > 0:
-            if userInfo == None:
-                return 'user doesnt exist'
-            if bcrypt.hashpw(passLogin, userInfo['password'].encode('utf-8')) == userInfo['password'].encode('utf-8'):
-                session['username'] = userInfo['username']
+            if userInfo is None:
+                session['authenticated'] = False
+                return redirect(url_for('login'))
+            if bcrypt.hashpw(passLogin, userInfo['Password'].encode('utf-8')) == userInfo['Password'].encode('utf-8'):
+                session['username'] = userInfo['Username']
                 session['authenticated'] = True
-                return 'login success'
+                return redirect(url_for('homePage'))
             else:
-                return 'login failed'
+                session['authenticated'] = False
+                return redirect(url_for('login'))
 
 
 @app.route('/logout')
 def logout():
+    #print(session.__getitem__('username'))
+    #session['username'] = ""
     session.clear()
-    return render_template('angajati.html')
+    #session['username']=""
+    #session['authenticated'] = False
+    return redirect(url_for('login'))
 
-@app.route('/visan')
-def visan():
-    return render_template('angajati_vis.html')
+
 if __name__ == '__main__':
 
     app.run(debug=True)
