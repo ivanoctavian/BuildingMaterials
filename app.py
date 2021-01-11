06b8e2baaa3f7cc779a01a1ff5036e5441f6822a
ivanoctavian <i.octavian900@gmail.com>
2021-01-12 00:48:49 +0200

    adauga material works just fine

--- app.py
+++ app.py
@@ -149,6 +149,30 @@ def deleteAngajat(id):
             return redirect(url_for('getAngajati'))
 #endregion
 
+#region Producatori
+@app.route('/producatori', methods=['GET', 'POST'])
+def getProducatori():
+    print("___app.py / getProducatori::producatori.html: Start getting data from DB.")
+    if request.method == 'GET':
+        if session.get('username') is None:
+            print("___app.py / getProducatori::producatori.html: Not logged in. Redirect to login.")
+            return redirect(url_for('login'))
+        if session['username']:
+            try:
+                curs = mysql.connection.cursor()
+                curs.execute('SELECT * FROM tblProducatori')
+                data = curs.fetchall()
+                # pentru a vedea atributele unui obiect:
+                #for ang in data:
+                 #   print(ang['idAngajat'])
+                print('___app.py / getProducatori::producatori.html: FETCH DONE.')
+                curs.close()
+            except Exception as e:
+                print('ERROR___app.py / getProducatori::producatori.html: EROARE FETCH____', e)
+
+            return render_template('producatori.html', producatori=data)
+#endregion
+
 #region Materiale CRUD ** NOT DONE **
 @app.route('/materiale', methods=['GET', 'POST'])
 def getMateriale():
@@ -186,10 +210,68 @@ def getMateriale():
             return render_template('materiale.html', materiale=data)
 #endregion
 
+@app.route('/adaugaMaterial', methods=['GET','POST'])
+def adaugaMaterial():
+    if request.method == "GET":
+        if session.get('username') is None:
+            print("___app.py / adaugaMaterial::adaugaMaterial.html: Not logged in. Redirect to login.")
+            return redirect(url_for('login'))
+        if session['username']:
+            print("___app.py / adaugaMaterial::adaugaMaterial.html: User %s logged in. Proceed to adaugaMaterial.html" % session.__getitem__('username'))
+            curs = mysql.connection.cursor()
+            query0 = 'SELECT Categorie FROM tblRaioane'
+            curs.execute(query0)
+            varianteCategorie = curs.fetchall()  # de trimis ca parametru
+            query1 = 'SELECT Denumire FROM tblProducatori'
+            curs.execute(query1)
+            varianteProducatori = curs.fetchall() # de trimis ca parametru
+            curs.close()
+            print(varianteProducatori)
+            print(varianteCategorie)
+            return render_template('adaugaMaterial.html', categoriiMaterial=varianteCategorie,
+                                   producatoriMaterial=varianteProducatori)
+
+    if request.method == 'POST':
+        print("___app.py / adaugaMaterial::adaugaMaterial.html: doPost STARTED")
+        numeProducator         = request.form['numeP']
+        categorie      = request.form['categorieM']
+        denumire      = request.form['denumireM']
+        unitati = request.form['unitatiM']
+        pret           = request.form['pretM']
+        garantie        = request.form['garantieM']
+
+        try:
+            curs = mysql.connection.cursor()
+
+            try:
+                query1 = 'SELECT idProducator FROM tblProducatori WHERE Denumire = %s'
+                query2 ='SELECT idRaion FROM tblRaioane WHERE Categorie = %s'
+                curs.execute(query1, [numeProducator])
+                producator = curs.fetchone()
+                idProducator = producator['idProducator']
+                curs.execute(query2, [categorie])
+                raion = curs.fetchone()
+                idRaion = raion['idRaion']
+                curs.execute(
+                    'INSERT INTO tblMateriale(ProducatorFK, RaionFK, Denumire, Unitati, PretRON, GarantieLUNI) VALUES (%s, %s, %s, %s, %s, %s)',
+                                             (idProducator, idRaion, denumire, unitati, pret,    garantie))
+                mysql.connection.commit()
+                print("___app.py / adaugaMaterial::adaugaMaterial.html: Material added successfully.")
+                curs.close()
+                return redirect(url_for('getMateriale'))
+            except Exception as e:
+                print("ERROR___app.py / adaugaMaterial::adaugaMaterial.html: ____EROARE ADAUGARE____::", e)
+                # print(traceback.print_exc())
+        except Exception as e:
+            print("ERROR___app.py / adaugaMaterial::adaugaMaterial.html: ____EROARE CONEXIUNE DB____")
+            print("ERROR___app.py / adaugaMaterial::adaugaMaterial.html: ____EROARE CONEXIUNE DB____::",e)
+
+        print("___app.py / adaugaMaterial::adaugaMaterial.html.html: ____doPost END")
+
 
 @app.route('/categorie/<string:categorie>', methods=['GET', 'POST'])
 def getMaterialeByCategorie(categorie):
-    print("___app.py / getMateriale::materiale.html: Start getting data from DB.")
+    print("___app.py / getMaterialeByCategorie::categorie.html: Start getting data from DB.")
     if request.method == 'GET':
         if session.get('username') is None:
             print("___app.py / getMaterialeByCategorie::categorie.html: Not logged in. Redirect to login.")
@@ -197,14 +279,7 @@ def getMaterialeByCategorie(categorie):
 
 
         if session['username']:
-            queryCastigator = "SELECT * FROM(SELECT tblMateriale.Denumire AS 'DenumireMaterial',tblProducatori.Denumire AS 'DenumireProducator'," \
-                              "tblMateriale.RaionFK AS 'Raion', Unitati, PretRON, GarantieLuni,tblAngajati.Nume AS 'NumeResponsabil'," \
-                              "tblAngajati.Telefon AS 'TelefonResponsabil' FROM tblResponsabiliRaioane" \
-                              " RIGHT JOIN tblMateriale ON tblResponsabiliRaioane.RaionFK=tblMateriale.RaionFK " \
-                              "LEFT JOIN tblAngajati ON tblResponsabiliRaioane.AngajatFK=tblAngajati.idAngajat" \
-                              " LEFT JOIN tblProducatori ON tblMateriale.ProducatorFK=tblProducatori.idProducator)" \
-                              "AS Tabel WHERE Tabel.DenumireProducator='Adeplast'"
-
+            curs = mysql.connection.cursor()
             queryFinal  =     "SELECT * FROM(SELECT idMaterial, tblMateriale.Denumire AS 'DenumireMaterial',tblProducatori.Denumire AS 'DenumireProducator'," \
                               "tblMateriale.RaionFK AS 'Raion', tblMateriale.Unitati AS 'Unitati', tblMateriale.PretRON AS 'PretRON', " \
                               "tblMateriale.GarantieLuni AS 'GarantieLuni',tblAngajati.Nume AS 'NumeResponsabil',tblAngajati.Telefon AS 'TelefonResponsabil'," \
@@ -216,16 +291,14 @@ def getMaterialeByCategorie(categorie):
                               " LEFT JOIN tblAngajati " \
                               "ON tblResponsabiliRaioane.AngajatFK=tblAngajati.idAngajat" \
                               " LEFT JOIN tblRaioane ON tblMateriale.RaionFK=tblRaioane.idRaion) AS Tabel" \
-                              "WHERE Tabel.Categorie = %s"
-            curs = mysql.connection.cursor()
-            sql1 = 'SELECT Categorie FROM tblRaioane WHERE Categorie=%s'
-            curs.execute(sql1,[categorie])
-            categorii = curs.fetchall()
-            sql = 'SELECT * FROM tblAngajati WHERE idAngajat=%s'
-            curs.execute(sql, [id])
-            res = curs.fetchone()
+                              "WHERE Tabel.Categorie = %s;"
+
+
+            a = " SELECT * FROM(SELECT idMaterial, tblMateriale.Denumire AS 'DenumireMaterial',tblProducatori.Denumire AS 'DenumireProducator',tblMateriale.RaionFK AS 'Raion', tblMateriale.Unitati AS 'Unitati', tblMateriale.PretRON AS 'PretRON', tblMateriale.GarantieLuni AS 'GarantieLuni',tblAngajati.Nume AS 'NumeResponsabil',tblAngajati.Telefon AS 'TelefonResponsabil',tblRaioane.Categorie AS 'Categorie' FROM tblResponsabiliRaioane RIGHT JOIN tblMateriale ON tblResponsabiliRaioane.RaionFK=tblMateriale.RaionFK LEFT JOIN tblProducatori   ON tblMateriale.ProducatorFK=tblProducatori.idProducator LEFT JOIN tblAngajati ON tblResponsabiliRaioane.AngajatFK=tblAngajati.idAngajat LEFT JOIN tblRaioane ON tblMateriale.RaionFK=tblRaioane.idRaion) AS Tabel WHERE Tabel.Categorie = %s   "
+            curs.execute(a, [categorie])
+            materialeDupaCategorie = curs.fetchall()
             curs.close()
-            return render_template("editAngajat.html", angajati=res)
+            return render_template("categorie.html", materiale=materialeDupaCategorie)
 
 
 
