1c37b984efacdec28b2bfa66571bfcb5d3377dac
ivanoctavian <i.octavian900@gmail.com>
2021-01-16 19:15:08 +0200

    new js for dropdown; style.css added

--- static/js/dropD.js
+++ static/js/dropD.js
@@ -1,12 +1,13 @@
-    $(".username").mouseenter(function() {
-        $(".logout").css('display','flex');
-    })
-    // $(".username").mouseleave(function() {
-    //     $(".logout").css('display','none');
-    // })
-    $(document).click(function(event) {
-        let $target = $(event.target);
-        if((!$target.closest('.username').length || !$target.closest('.logout').length)){
-        $('.logout').css('display','none');
+$(".username").mouseenter(function () {
+  $(".logout").css("display", "block");
+});
+
+$(document).click(function (event) {
+  let $target = $(event.target);
+  if (
+    !$target.closest(".username").length &&
+    !$target.closest(".logout").length
+  ) {
+    $(".logout").css("display", "none");
   }
 });
